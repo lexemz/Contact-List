2c08f25117652fec9a99a019964013929338434c
Igor Buzykin <lexemz@gmail.com>
2021-10-02 21:54:00 +0300

    Initial setup

---
+++ Contact List/Service/NetworkMaganer.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkMaganer.swift
+//  Contact List
+//
+//  Created by Igor on 02.10.2021.
+//
+
+import Foundation
+
+class NetwokManager {
+    static let shared = NetwokManager()
+    
+    private init() {}
+    
+    // 
+}
